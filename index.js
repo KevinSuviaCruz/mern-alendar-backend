db1fbc2517364abb56601043d87ce095bc88599f
kevoff <suviacruzfer@gmail.com>
2023-07-17 19:28:07 -0300

    correct the rute

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ app.use('/api/auth', require('./routes/auth'))
 // TODO: CRUD // Eventos
 app.use('/api/events', require('./routes/events'))
 
+// Si no ingresa a la rutas de arriba que venga a buscar esto
+app.get('*', (req, res) => {
+  // eslint-disable-next-line n/no-path-concat
+  res.sendFile(__dirname + '/public/index.html')
+})
+
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
   console.log(`Servido corriendo en puerto ${process.env.PORT}`)
